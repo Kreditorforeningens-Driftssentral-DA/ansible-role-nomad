9d387c249210d03955f945cc52e905c5c87477cd
Rune Rønneseth <rune.ronneseth@kred.no>
2019-12-19 12:31:51 +0100

    Missing firewalld in molecule image
    ..checking..

--- molecule.ps1
+++ molecule.ps1
@@ -9,7 +9,7 @@ docker run --rm -it `
   -v "$($pwd)/$($role):/tmp/$($role)" `
   -v "/var/run/docker.sock:/var/run/docker.sock" `
   -w "/tmp/$($role)" `
-  -e JOB_DOCKER_IMAGE="$($distro)" `
-  -e JOB_DOCKER_COMMAND="$($cmd)" `
+  -e MOLECULE_IMAGE="$($distro)" `
+  -e MOLECULE_COMMAND="$($cmd)" `
   quay.io/ansible/molecule:latest `
   molecule "$($command)"
